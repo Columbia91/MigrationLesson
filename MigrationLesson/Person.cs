c55062099ba889a8a9abda302fb684845e757072
Columbia91 <alpha.z91@mail.ru>
2019-04-18 19:55:34 +0600

    Добавьте файлы проекта.

---
+++ MigrationLesson/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationLesson
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ MigrationLesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                FullName = "Пушкин А.С.",
+                Gender = "мужской",
+                Age = 86
+            };
+            
+            using(var context = new DataContext())
+            {
+                context.People.Add(person);
+                context.SaveChanges();
+            }
+        }
+    }
+}
